18e8b061e2cb9b78aa79f575f6c69993771cefe6
Xzyaihni <Fxlictx996@gmail.com>
2023-03-17 16:12:29 +0800

    fixed aspect ratio, this time for sure (Clueless)

--- src/main.rs
+++ src/main.rs
@@ -354,7 +354,7 @@ fn main()
 
     let canvas_aspect = max_width / max_height;
 
-    let width = image_width as f64 * canvas_aspect;
+    let width = image_width as f64 / canvas_aspect;
     let height = image_height as f64;
 
     let (width, height) = if width > height
